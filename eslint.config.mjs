36fb1bae3fe2d56cddafc3351b7380a2685b63be
Julie - Mathman <casperschorr06@gmail.com>
2026-09-15 18:04:39 -0400

    Refactor ESLint configuration to use defineConfig for improved type safety

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
+import { defineConfig } from "eslint/config";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
     {
         ignores: ["dist/**", "node_modules/**"]
     },
